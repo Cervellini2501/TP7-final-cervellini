e4593058cd4a83ac54bfa9f34d73cdcf6d0dbcf0
Valentina Cervellini <2200169@ucc.edu.ar>
2025-12-07 01:44:01 -0300

    changes cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,23 @@
-const { defineConfig } = require("cypress");
+// cypress.config.js en la raíz del repo
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
+    // Hook de configuración (si mañana querés agregar tasks, etc.)
     setupNodeEvents(on, config) {
+      // por ahora no hacemos nada especial
       return config;
     },
+
+    // Reporter JUnit para que Azure lea los XML
     reporter: "junit",
     reporterOptions: {
-      mochaFile: "cypress/results/results-[hash].xml",
+      mochaFile: "cypress/results/junit-[hash].xml",
       toConsole: true,
     },
-    baseUrl: "http://localhost:3000" 
+
+    // Base URL por defecto (se sobreescribe en el pipeline con --config)
+    baseUrl: "http://localhost:3000",
   },
+
   experimentalStudio: true,
-});
+};
